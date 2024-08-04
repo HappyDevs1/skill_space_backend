9748b5763d74da4cdd477304e743e11fda9e47e5
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-08-04 15:17:21 +0200

    feat: :sparkles: adding the route for getting service by filter

--- src/routes/serviceRoute.ts
+++ src/routes/serviceRoute.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { Request, Response } from "express";
 const router = express.Router();
-import { createService, getAllServices, updateService, getServiceById, deleteService } from "../controllers/serviceController";
+import { createService, getAllServices, updateService, getServiceById, deleteService, getServiceByFilter } from "../controllers/serviceController";
 
 router.post("/create", createService);
 router.get("/services", getAllServices);
+router.get("/filter", getServiceByFilter);
 router.put("/:id/edit", updateService);
 router.get("/:id", getServiceById);
 router.delete("/:id/delete", deleteService);
