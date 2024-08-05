e72bb6b85b4284f76ddebf3fa6f60dc4b362aee8
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-08-05 19:25:03 +0200

    style: :lipstick: adding capitals to my models value

--- src/models/serviceModel.ts
+++ src/models/serviceModel.ts
@@ -6,8 +6,8 @@ export interface IService extends Document {
   description: string;
   price: number;
   location: "Gauteng" | "Eastern-Cape" | "Mpumalanga" | "Free-State" | "Limpopo" | "North-West" | "Nothern-Cape" | "Western-Cape" | "KwaZulu-Natal" | "Remote";
-  level: `"internship" | "junior" | "mid-level" | "senior"`;
-  department: "finance" | "technology" | "healthcare" | "real-estate" | "construction";
+  level: "Internship" | "Junior" | "Mid-level" | "Senior";
+  department: "Finance" | "Technology" | "Healthcare" | "Real-estate" | "Construction";
   featured: boolean;
   createdAt: Date;
   updatedAt: Date;
@@ -19,8 +19,8 @@ const ServiceSchema: Schema = new Schema({
   description: { type: String, required: true },
   price: { type: Number, required: true },
   location: { type: String, required: true, enum: ["Gauteng", "Eastern-Cape", "Mpumalanga", "Free-State", "Limpopo", "North-West", "Nothern-Cape", "Western-Cape", "KwaZulu-Natal", "Remote"] },
-  level: { type: String, required: true, enum: ["internship", "junior", "mid-level", "senior"]},
-  department: { type: String, required: true, enum: ["finance", "technology", "healthcare", "real-estate", "construction"]},
+  level: { type: String, required: true, enum: ["Internship", "Junior", "Mid-level", "Senior"]},
+  department: { type: String, required: true, enum: ["Finance", "Technology", "Healthcare", "Real-estate", "Construction"]},
   featured: { type: Boolean, required: true, default: false },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
