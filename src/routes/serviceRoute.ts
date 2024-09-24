1ba604eee21c2282e28da783e2d95ca906e8ffc2
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-09-24 02:26:13 +0200

    feat: :sparkles: adding the authenticate middleware to protect sensitive HTTP routes

--- src/routes/serviceRoute.ts
+++ src/routes/serviceRoute.ts
@@ -4,13 +4,13 @@ const router = express.Router();
 import { createService, getAllServices, updateService, getServiceById, deleteService, getServiceByFilter, getFeaturedServices } from "../controllers/serviceController";
 import { authenticateMiddleware } from "../middleware/authMiddleware";
 
-router.post("/create/:freelancerId", createService);
+router.post("/:freelancerId/create", authenticateMiddleware, createService);
 router.get("/services", getAllServices);
 router.get("/filter", getServiceByFilter);
 router.get("/featured", getFeaturedServices);
-router.put("/:id/edit", updateService);
+router.put("/:id/edit", authenticateMiddleware, updateService);
 router.get("/:id", getServiceById);
-router.delete("/:id/delete", authenticateMiddleware, deleteService);
+router.delete("/:id/delete", authenticateMiddleware, authenticateMiddleware, deleteService);
 
 router.use("/", (req: Request, res: Response) => {
   res.json({ message: "Service API" });
