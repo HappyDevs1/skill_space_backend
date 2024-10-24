6600900b0861ec91df2d11cd54b743d4ac151e97
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-24 19:33:57 +0200

    feat: :sparkles: adding get featured user route

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { Request, Response } from "express";
-import { createUser, getUsers, findUser, loginUser, editUser, deleteUser } from "../controllers/userController";
+import { createUser, getUsers, findUser, getFeaturedUser, loginUser, editUser, deleteUser } from "../controllers/userController";
 const router = express.Router();
 
 router.post("/create", createUser);
 router.get("/users", getUsers);
 router.get("/:id", findUser);
+router.get("/featured", getFeaturedUser);
 router.put("/:id/edit", editUser);
 router.delete("/:id/delete", deleteUser);
 router.post("/login", loginUser);
