5afbd549f17edc0545ccd8c10ad3d5be2f1736c8
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-11-01 11:40:33 +0200

    feat: :sparkles: updating the user model to not include role since it is no longer necessary

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -5,7 +5,6 @@ export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
-  role: "client" | "freelancer";
   profilePicture: string;
   createdAt: Date;
   updatedAt: Date;
@@ -15,7 +14,6 @@ const UserSchema: Schema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: true, enum: ["client", "freelancer"]},
   profilePicture: {
     type: String,
     default: "https://i0.wp.com/vssmn.org/wp-content/uploads/2018/12/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png?fit=860%2C681&ssl=1"
