072bf87ed26ae339e16fc0c1ffc50f8e4a6d0acf
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-12-14 10:08:26 +0200

    feat: :sparkles: updating the application route to use the other new controller function

--- src/routes/applicationRoute.ts
+++ src/routes/applicationRoute.ts
@@ -1,15 +1,36 @@
 import express from "express";
 import { Request, Response } from "express";
 const router = express.Router();
-import { createApplication, getAllApplication, getApplicationById, editApplication, deleteApplication } from "../controllers/applicationController";
+import { getAllApplication, getApplicationById, editApplication, deleteApplication, createApplication } from "../controllers/applicationController";
+import multer from "multer";
+import path from "path";
+import fs from "fs";
 
-router.post("/create", createApplication);
+// Ensure "public/img" directory exists
+if (!fs.existsSync("public/img")) {
+  fs.mkdirSync("public/img", { recursive: true });
+}
+
+// Multer configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/img");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post("/upload", upload.single("file"), createApplication );
+// router.post("/create", upload.single("file"), createApplication);
 router.get("/applications", getAllApplication);
 router.get("/:id", getApplicationById);
 router.put("/:id/edit", editApplication);
 router.delete("/:id/delete", deleteApplication);
 
-router.use("/" , (req: Request, res: Response) => {
+router.use("*" , (req: Request, res: Response) => {
   res.status(200).json({ message: "Application API" });
 });
 
