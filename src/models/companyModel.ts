131b069433d01bedbfe4c0059574a75f91dde713
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-12-23 12:06:45 +0200

    style: :lipstick: adding website section to the model of the company

--- src/models/companyModel.ts
+++ src/models/companyModel.ts
@@ -7,6 +7,7 @@ export interface ICompany extends Document {
   password: string;
   about: string;
   perksAndBenefits: string;
+  website: string;
   featured: boolean;
   profilePicture: string;
   services: mongoose.Types.ObjectId[];
@@ -18,16 +19,18 @@ const CompanySchema: Schema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  about: { type: String, required: true},
+  about: { type: String, required: true },
   perksAndBenefits: { type: String, required: false },
-  featured: { type: Boolean, required: true},
+  website: { type: String, required: false },
+  featured: { type: Boolean, required: true },
   profilePicture: {
     type: String,
-    default: "https://i0.wp.com/vssmn.org/wp-content/uploads/2018/12/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png?fit=860%2C681&ssl=1"
+    default:
+      "https://i0.wp.com/vssmn.org/wp-content/uploads/2018/12/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png?fit=860%2C681&ssl=1",
   },
   services: [{ type: mongoose.Types.ObjectId, ref: "Service", default: [] }],
-  createdAt: { type: Date, default: Date.now},
-  updatedAt: { type: Date, default: Date.now}
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 const Company = mongoose.model<ICompany>("Company", CompanySchema);
